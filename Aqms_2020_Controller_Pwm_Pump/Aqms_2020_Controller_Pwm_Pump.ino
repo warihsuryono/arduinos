13816a4b7a3f0cdbde0e91d887c645d99c22ba14
warih suryono <warih_suryono@yahoo.com>
2020-05-06 11:48:47 +0700

    aaa

--- Aqms_2020_Controller_Pwm_Pump/Aqms_2020_Controller_Pwm_Pump.ino
+++ Aqms_2020_Controller_Pwm_Pump/Aqms_2020_Controller_Pwm_Pump.ino
@@ -3,10 +3,7 @@ void setup() {
     Serial.begin(9600);
     pinMode(5, OUTPUT); //PWM Pump 1
     pinMode(6, OUTPUT); //PWM Pump 2
-//    for (int i = 0; i <= 200; i+=5) {
-//        analogWrite(5,i);
-//        delay(100);
-//    }
+    Serial.println("Ready");
 }
 
 void loop() {
@@ -17,11 +14,17 @@ void loop() {
             if(in <= 100){
                 analogWrite(6,0);
                 in = map(in, 0, 100, 0, 255);
-                analogWrite(5,in);
+                for (int i = 0; i <= in; i+=1) {
+                    analogWrite(5,i);
+                    delay(15);
+                }
             } else {
                 analogWrite(5,0);
                 in = map(in, 101, 200, 0, 255);
-                analogWrite(6,in);
+                for (int i = 0; i <= in; i+=1) {
+                    analogWrite(6,i);
+                    delay(15);
+                }
             }
         }
     }
