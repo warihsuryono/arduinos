2ed924fb92d1eb4dfb01bc300ffaa1dd4172b41f
warih suryono <warih_suryono@yahoo.com>
2020-12-07 13:41:27 +0700

    pumptimer

--- PumpTimer/PumpTimer.ino
+++ PumpTimer/PumpTimer.ino
@@ -1,26 +1,21 @@
 
-const int buttonPin = 3;
 const int voltagePin =  4;
 const int pumpPin =  5;
 const int switchTime =  6*60*60;
 
-int buttonState = 0;
 int pumpState = 0;
 int timerCounter = 0;
 
 void setup() {
   pinMode(voltagePin, OUTPUT);
   pinMode(pumpPin, OUTPUT);
-  pinMode(buttonPin, INPUT);
-  digitalWrite(voltagePin, LOW);
+  digitalWrite(voltagePin, HIGH);
   digitalWrite(pumpPin, LOW);
   pumpState = 0;
   timerCounter = 0;
 }
 
 void loop() {
-  buttonState = digitalRead(buttonPin);
-  //if (buttonState == LOW) { switchPump(); }
   if(timerCounter >= switchTime){ switchPump(); }
   timerCounter++;
   delay(1000);
